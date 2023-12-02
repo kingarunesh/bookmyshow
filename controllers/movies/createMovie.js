2127c9a00cfbf404a0d625220e77c8c4fdae30a7
Arunesh kumar <aruneshkumar.king@gmail.com>
2023-12-02 23:10:05 +0530

    7.1 : route middleware - body checking

--- controllers/movies/createMovie.js
+++ controllers/movies/createMovie.js
@@ -2,7 +2,18 @@ import fs from "fs";
 
 const movies = JSON.parse(fs.readFileSync("./data/movies.json", "utf-8"));
 
-export default (req, res) => {
+export const checkBody = (req, res, next) => {
+    if (!req.body.title || !req.body.duration || !req.body.price) {
+        return res.status(401).json({
+            status: "fail",
+            message: "Content Missing",
+        });
+    }
+
+    next();
+};
+
+export const createMovie = (req, res) => {
     const newMovies = { id: movies.length, ...req.body };
     movies.push(newMovies);
 
